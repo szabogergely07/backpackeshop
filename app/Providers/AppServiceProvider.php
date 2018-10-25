42f45b77c9adef74376f9f28584ae70ac9ed7685
szabogergely07 <szabogergely07@gmail.com>
2018-10-25 14:23:24 +0200

    fix mixed content issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-       
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
